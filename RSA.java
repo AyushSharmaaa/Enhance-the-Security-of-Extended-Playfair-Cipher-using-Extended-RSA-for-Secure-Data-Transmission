bfe87d046b421b773e73e86442e56f159a7d158a
Ayush Sharma <67716805+AyushSharmaaa@users.noreply.github.com>
2023-02-14 22:41:48 +0530

    Update in code

--- RSA.java
+++ RSA.java
@@ -22,7 +22,6 @@ public class RSA {
         return new BigInteger(1,b);
     }
 
-
     /**
      *  Convert a BigInteger into a string of ASCII characters.  Each byte
      *  in the integer is simply converted into the corresponding ASCII code.
@@ -42,6 +41,7 @@ public class RSA {
      *  Then that integer, x, is encoded by computing  x^e (mod N).
      */
     public static BigInteger[] encode(String plaintext, BigInteger N, BigInteger e) {
+        String pt = plaintext;
         int charsperchunk = (N.bitLength()-1)/8;
         while (plaintext.length() % charsperchunk != 0)
             plaintext += ' ';
@@ -49,7 +49,7 @@ public class RSA {
         BigInteger[] c = new BigInteger[chunks];
         for (int i = 0; i < chunks; i++) {
             String s = plaintext.substring(charsperchunk*i,charsperchunk*(i+1));
-            c[i] = string2int(s);
+            c[i] = string2int(pt);
             c[i] = c[i].modPow(e,N);
         }
         return c;
@@ -71,71 +71,9 @@ public class RSA {
 
     public static void main(String[] str) throws java.io.IOException {
 
-        Random random = new Random();
-        System.out.println("\n\nComputing public key (N,e) and private key (N,d):");
 
-        // Choose two large primes p and q, let N  = pq, and let p1p1 = (p-1)(q-1).
 
-        System.out.print("Computing p 1 ... ");
-        System.out.flush();
-        BigInteger p1 = new BigInteger(bits, 50, random);
-        System.out.println(p1);
-        System.out.print("Computing p 2... ");
-        System.out.flush();
-        BigInteger p2 = new BigInteger(bits, 50, random);
-        System.out.println(p2);
-        System.out.print("Computing q 1 ... ");
-        System.out.flush();
-        BigInteger q1 = new BigInteger(bits, 50, random);
-        System.out.println(q1);
-        System.out.print("Computing q 2 ... ");
-        System.out.flush();
-        BigInteger q2 = new BigInteger(bits, 50, random);
-        System.out.println(q2);
-        BigInteger N = p1.multiply(p2.multiply(q1.multiply(q2)));
-        System.out.println("N = p1*p2*q1*q2 is       " + N);
-        BigInteger p1phi = p1.subtract(BigInteger.ONE);
-        BigInteger p2phi = p2.subtract(BigInteger.ONE);
-        BigInteger q1phi = q1.subtract(BigInteger.ONE);
-        BigInteger q2phi = q2.subtract(BigInteger.ONE);
-        BigInteger phi_N = p1phi.multiply(p2phi.multiply(q1phi.multiply(q2phi)));
-        System.out.println("phi(N) = (p1-1)(p2-1)(q1-1)(q2-1) is   " + phi_N);
-        System.out.println();
-
-        // Choose numbers e and d such that e is prime and ed = 1 mod N.
-
-        BigInteger e = new BigInteger("" + 0x10001);
-        System.out.println("Using e =       " + e);
-        System.out.print("Computing d ... ");
-        BigInteger d = e.modInverse(phi_N);
-        System.out.println(d);
-
-        // Now, the public key is the pair (N,d) and the private key
-        // is the pair (N,e).  Do some encryptions and decryptions.
-        // The user enters text that is encoded into an array of
-        // integers.  (Use an array, not a single integer, since
-        // the algorithm can only deals with a certain number of
-        // characters at a time.)  Then this array is decoded to
-        // give (if the algorithm is working) the original text.
-        Scanner scanner = new Scanner(System.in);
-
-            System.out.println("\n\nEnter plaintext, press return to end: ");
-            System.out.print("     ");
-            StringBuffer b = new StringBuffer(scanner.nextLine());
-
-//            while (true) {
-//                int ch = System.in.read();
-//                if (ch == '\n' || ch == -1)
-//                    break;
-//                b.append((char)ch);
-//            }
-            String s = b.toString();
-            if (s.trim().length() == 0) {
-                System.out.println("length 0");
-            }
-
-            System.out.println();
-            System.out.println("Encoded Text, computed with RSA:");
+            /**System.out.println("Encoded Text, computed with RSA:");
             BigInteger[] cyphertext = encode(s,N,e);
             for (int i = 0; i < cyphertext.length; i++) {
                 System.out.println("     " + cyphertext[i]);
@@ -145,7 +83,7 @@ public class RSA {
 
             System.out.println("Decoded Text, computed with RSA:");
             String plaintext = decode(cyphertext,N,d);
-            System.out.println("     " + plaintext);
+            System.out.println("     " + plaintext);*/
 
     }
 
@@ -153,7 +91,6 @@ public class RSA {
 
 
 /**
-
 Sample output:
 
 
